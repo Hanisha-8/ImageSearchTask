909ed6eef060fa737668c27d352123ea960b9636
Hanisha Goyal <hanisha@Hanishas-Air.Dlink>
2019-04-14 21:01:12 +0530

    Initial structure

---
+++ ImageSearch/Helper/UIViewController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Extension.swift
+//  ImageSearch
+//
+//  Created by Hanisha Goyal on 4/13/19.
+//  Copyright © 2019 Hanisha Goyal. All rights reserved.
+//
+
+import Foundation
---
+++ ImageSearch/View/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageSearch
+//
+//  Created by Hanisha Goyal on 4/12/19.
+//  Copyright © 2019 Hanisha Goyal. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
